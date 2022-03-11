a17b495e6ff3e9a8555b82483154bbce558991a2
AppLibrary <Ebariyiga@outlook.com>
2022-03-11 16:47:58 -0500

    Initial Commit Of SortNames

---
+++ ConsoleApps/SortNamesDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuadum
+{
+    public class SortNamesDb : ISortNames
+    {
+        void ISortNames.RandomSort(string fileOne, string fileTwo)
+        {
+            //TODO
+            //throw new NotImplementedException(
+        }
+
+        void ISortNames.Write(string fileOne)
+        {
+            //TODO
+            //throw new NotImplementedException();
+        }
+    }
+}
